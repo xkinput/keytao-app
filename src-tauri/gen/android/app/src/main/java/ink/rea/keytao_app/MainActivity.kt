e856e8ca6efac0851a4a1d9135b92277896300e9
rea <hi@rea.ink>
2026-06-05 22:54:18 +0800

    fix(android): initialize PluginManager on activity creation

--- src-tauri/gen/android/app/src/main/java/ink/rea/keytao_app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/ink/rea/keytao_app/MainActivity.kt
@@ -2,9 +2,11 @@ package ink.rea.keytao_app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import app.tauri.plugin.PluginManager
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    PluginManager.onActivityCreate(this)
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
   }
